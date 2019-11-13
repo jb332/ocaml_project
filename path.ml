e0daad05d03e25a966455a1e08e00ac045fbecc3
M. Bigue-Colas Remy <rbigue-c@insa-20549>
2019-11-13 18:15:25 +0100

    programme pour la fois prochaine

---
+++ path.ml
@@ -0,0 +1,6 @@
+(*DANS LE PROCHAIN EPISODE ::: 
+
+1- Implementer le modulte path (type record : un noeud depart -> une liste de noeuds)
+2- Methode de parcours en largeur qui renvoie un chemin de s vers p
+3- Type record contenant flot et capacite 
+  4- Deroulement de l'algorithme ... *)
