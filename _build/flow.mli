910f483d849684d177f49ea9412c82c3915933b4
jean-baptiste <jb@debian-jb>
2019-11-19 16:12:42 +0100

    commentaires

--- _build/flow.mli
+++ _build/flow.mli
@@ -2,24 +2,8 @@ open Graph
 open Tools
 
 type flow_lbl
-type diff_lbl = int
-
 
 val flow_of_string: string -> flow_lbl
 val string_of_flow: flow_lbl -> string
-
-val diff_of_string: string -> diff_lbl
-val string_of_diff: diff_lbl -> string
-
-val generate_diff_gr: flow_lbl graph -> diff_lbl graph
-
-
-
-type path
-
-val dfs: diff_lbl graph -> id -> id -> path
-val print_path: path -> unit
-
-val update_flow_graph: flow_lbl graph -> path -> flow_lbl graph
 
 val ford_fulkerson: flow_lbl graph -> id -> id -> flow_lbl graph
--- flow.ml
+++ flow.ml
@@ -1,6 +1,7 @@
 open Graph
 open Tools
 
+(* Label types *)
 type flow_lbl = {
     flow: int;
     capacity: int
@@ -8,7 +9,15 @@ type flow_lbl = {
 
 type diff_lbl = int
 
-(* Conversions type string type flow (flot + capacite) *)
+(* Path type *)
+type path = diff_lbl out_arcs
+
+
+(*************************
+**** Difference graph ****
+*************************)
+
+(* Conversion functions between label types and type string *)
 let flow_of_string str =
     let exploded_str = String.split_on_char '/' str in
     match exploded_str with
@@ -21,13 +30,11 @@ let flow_of_string str =
 
 let string_of_flow flbl = (string_of_int flbl.flow) ^ "/" ^ (string_of_int flbl.capacity) 
 
-(* Conversions type string type diff (ecart) *)
 let diff_of_string str = int_of_string str
 
 let string_of_diff dlbl = string_of_int dlbl
 
-
-(* Création d'un arc d'écart positif *)
+(* Create a positive difference bow *)
 let get_ecart_pos flbl = (flbl.capacity - flbl.flow)
 let generate_diff_arc_pos gr_acu id1 id2 flbl =
 	let ecart_pos = get_ecart_pos flbl in
@@ -35,7 +42,7 @@ let generate_diff_arc_pos gr_acu id1 id2 flbl =
 		| 0 -> gr_acu
 		| _ -> new_arc gr_acu id1 id2 ecart_pos
 
-(* Création d'un arc d'écart négatif *)
+(* Create a negative difference bow *)
 let get_ecart_neg flbl = flbl.flow
 let generate_diff_arc_neg gr_acu id1 id2 flbl =
 	let ecart_neg = get_ecart_neg flbl in
@@ -43,7 +50,7 @@ let generate_diff_arc_neg gr_acu id1 id2 flbl =
 		| 0 -> gr_acu
 		| _ -> new_arc gr_acu id2 id1 ecart_neg
 
-(* Génération d'un graphe d'écart à partir d'un graph de flots *)
+(* Generate a difference graph from a flow graph *)
 let generate_diff_gr fgr =
     let dgr_nodes = clone_nodes fgr in
     let dgr_arcs_pos = e_fold fgr generate_diff_arc_pos dgr_nodes in
@@ -51,27 +58,23 @@ let generate_diff_gr fgr =
     dgr_arcs_neg
 
 
-(* Path *)
-type path = diff_lbl out_arcs
+(**************************************
+**** Depth-First Search Algorithm *****
+**************************************)
 
-
-let rec print_path path = match path with
-	| [] -> Printf.printf "\n%!"
-	| (id, _) :: rest -> Printf.printf "%d " id; print_path rest
-
-
-(* Parcours en profondeur *)
-(* Indique si un arc (son sommet destination) est marqué *)
+(* Returns true if a vertex is marked *)
 let is_marked arc mark_list =
 	match arc with
 		| (id, _) -> List.mem id mark_list
 
+(* Marks a vertex *)
 let mark id mark_list = id :: mark_list
 
 (* Enpiler un arc (son sommet destination) *)
 let push pile arc = arc :: pile
 
-(* Dépiler un arc (som sommet destination) *)
+(* Pop the stack *)
+(* To be used in the dfs algortihm or in the functions it calls *)
 let pop pile =
 	match pile with
 		| [] -> raise (Failure "empty pile")
@@ -81,6 +84,8 @@ let read pile =
 	match List.hd pile with
 		| (id, _) -> id
 
+(* Get all unmarked out arcs of a node *)
+(* To be used in the following dfs algorithm *)
 let get_unmarked_neighbor gr id mark_list =
 	let rec loop arcs mark_list =
 		match arcs with
@@ -92,6 +97,7 @@ let get_unmarked_neighbor gr id mark_list =
 	in
 	loop (out_arcs gr id) mark_list
 
+(* Depth-first search algorithm *)
 let dfs gr id_src id_dst =
 	let rec loop gr id_dst pile mark_list =
 		match pile with
@@ -100,7 +106,7 @@ let dfs gr id_src id_dst =
 				begin
 					let id = read pile in
 					let new_mark_list = mark id mark_list in
-					match get_unmarked_neighbor gr id mark_list with
+					match get_unmarked_neighbor gr id new_mark_list with
 						| Some (id, dlbl) when id = id_dst -> List.rev (push pile (id, dlbl))
 						| Some arc -> loop gr id_dst (push pile arc) new_mark_list
 						| None -> loop gr id_dst (pop pile) new_mark_list
@@ -108,7 +114,25 @@ let dfs gr id_src id_dst =
 	in
 	loop gr id_dst [(id_src, 0)] []
 
-(* Minimum path *)
+(* Print a path *)
+let print_path path = match path with
+	| [] -> Printf.printf "aucun chemin\n%!"
+	| [_] -> Printf.printf "source = destination\n%!"
+	| (id_src, _) :: path_rest ->
+		let rec loop arcs = match arcs with
+			| [] -> Printf.printf "\n%!"
+			| (id, _) :: rest -> Printf.printf " -> %d%!" id; loop rest
+		in
+		Printf.printf "chemin : %d%!" id_src;
+		loop path_rest
+
+
+(*********************************
+**** Ford Fulkerson algorithm ****
+*********************************)
+
+(* Get the minimum flow of the arcs in the path *)
+(* To be used in "update_flow_graph" *)
 let get_min_cost_path path =
 	match path with
 		| [] -> raise (Failure "empty path")
@@ -126,16 +150,20 @@ let get_min_cost_path path =
 			in
 			loop dlbl other_arcs
 
+(* Increment flow by n *)
 let add_flow flbl n = {
 	flow = flbl.flow + n;
 	capacity = flbl.capacity
 }
 
+(* Decrement flow by n *)
 let substract_flow flbl n = {
 	flow = flbl.flow - n;
 	capacity = flbl.capacity
 }
 
+(* Check if the current arc is in the path and increment it with n if it is *)
+(* To be used with "e_fold" in the "update_flow_graph" function *)
 let update_flow_arc path n gr id1 id2 flbl =
 	let rec loop arcs = match arcs with
 		| (pid1, _) :: (pid2, _) :: _ when (pid1 = id1) && (pid2 = id2) -> new_arc gr id1 id2 (add_flow flbl n)
@@ -145,10 +173,18 @@ let update_flow_arc path n gr id1 id2 flbl =
 	in
 	loop path
 
+(* Update the graph by incrementing or decrementing the flow of the arcs in the path *)
+(* To be used in "ford_fulkerson" *)
 let update_flow_graph gr path =
 	let n = get_min_cost_path path in
+	(* start print zone *)
+	Printf.printf "\n%!";
+	print_path path;
+	Printf.printf "flot minimum : %d\n%!" n;
+	(* end print zone *)
 	e_fold gr (update_flow_arc path n) gr
 
+(* Max flow algorithm *)
 let ford_fulkerson gr id_src id_dst =
 	let rec loop fgr = 
 		let dgr = generate_diff_gr fgr in
